4b7d63424db66755e8db97d2ddfe631e43d9d9d5
Ariel Zhen <zhenpingping@live.com>
2018-05-26 19:55:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,13 @@
 def oxford_comma(array)
-  arr = array.pop()
-  array.join(', ') + " and #{arr}"
+  result = ""
+  if array.size == 1
+    result = array.join()
+  elsif array.size == 2
+    arr = array.pop()
+    result = array.join(', ') + " and #{arr}"
+  else
+    arr = array.pop()
+    array.join(', ') + ", and #{arr}"
+  end
+  return result
 end
